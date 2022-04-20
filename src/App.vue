849fd2267938bf8a1c2322ed96401b7e5f4c4c6d
dinoarpa98 <dinoarpa98@gmail.com>
2022-04-20 17:11:14 +0200

    p1 importing axios

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+
+  <div>
+    <header>
+      <HeaderComp />
+    </header>
+
+    <main>
+      <div class="container">
+      
+      </div>
+    </main>
+
+  </div>
+
+
+</template>
+
+<script>
+  import HeaderComp from './components/HeaderComp.vue'
+  import "bootstrap"
+  import axios from 'axios'
+
+  export default {
+    name: 'App',
+    components: {
+      HeaderComp
+    }
+  }
+</script>
+
+
+<style lang="scss">
+  @import "bootstrap/dist/css/bootstrap.min.css";
+  
+
+  .container{
+    background-color: #1E2D3B;
+  }
+</style>
---
+++ src/components/HeaderComp.vue
@@ -0,0 +1,26 @@
+<template>
+  <header>
+    <div class="icon">
+     <img src="../assets/spotify-logo-png-7053.png" alt="">
+    </div>
+  </header>
+  
+</template>
+
+<script>
+export default {
+  //Cambiare il nome con quello del componente creato
+  name: 'HeaderComp',
+}
+</script>
+
+<style scoped lang="scss">
+.icon {
+    background-color: #2E3A46;
+    padding: 20px;
+    img{
+     width: 50px;
+}
+    
+ }
+</style>
